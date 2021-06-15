a49b3959179d04561755a292f5454e3ef944ff32
aayush pradhan <78127930+aayush1123@users.noreply.github.com>
2021-06-15 09:37:22 +0545

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,7 @@
+#tuples is enclosed in a small bracket()
+#Tuples and list are similar
+tup=(1,3,76,8,"hi")#this is a tuple which is immutable(can not be edited )
+print(tup[-1])
+#tup.append("hello") In the list we can add something by using append but in tuples we can't
+t=[12,3,4,5,6,6]
+print(t[0],t[3],t[2])
